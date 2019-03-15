d85ad03bf98fda3675d13ae5699a130ff3e74a01
sherlin <herlin.simon@gmail.com>
2019-03-15 11:17:13 +0100

    [INIT] project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <index></index>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import index from './components/index'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    index
   }
 }
 </script>
